c715a09a2f69f8ef4a21e892ffd59b2c7252cd6b
Mathieu Gouin <mgouin@gmail.com>
2023-12-28 19:51:18 +0000

    man install from source

--- man_install.sh
+++ man_install.sh
@@ -1,3 +1,7 @@
 HUGO_VERSION=0.121.1
-wget -O hugo.deb https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.deb
-sudo dpkg -i hugo.deb
+#wget -O hugo.deb https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.deb
+#sudo dpkg -i hugo.deb
+
+wget -O hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
+mkdir bin
+tar -xzvf hugo.tar.gz --directory bin

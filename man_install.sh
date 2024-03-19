cf693a1756e8e84e417baf6ac6312c69f9654d74
Mathieu Gouin <mgouin@gmail.com>
2024-03-19 01:34:23 +0000

    Test w/ new Hugo 0.124.0

--- man_install.sh
+++ man_install.sh
@@ -1,4 +1,4 @@
-HUGO_VERSION=0.121.1
+HUGO_VERSION=0.124.0
 
 # Deb not working on github codespace.
 #wget -O hugo.deb https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.deb

7cfdade15277e90ec2cecff7585f5b9045a364e0
Mathieu Gouin <mgouin@gmail.com>
2024-04-09 01:01:07 +0000

    shebang

--- man_install.sh
+++ man_install.sh
@@ -1,9 +1,12 @@
+#!/usr/bin/bash
+
 HUGO_VERSION=0.124.0
 
 # Deb not working on github codespace.
 #wget -O hugo.deb https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.deb
 #sudo dpkg -i hugo.deb
 
+rm -dRf bin
 wget -O hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
 mkdir bin
 tar -xzvf hugo.tar.gz --directory bin
